4529029c80f4d958c692e3376043903ef9d4b72a
Damianos P. Melidis <damianosmel@gmail.com>
2025-12-05 16:56:05 +0200

    Show examples on jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
 const App = () => {
-  return <div className='text-5xl'>App</div>;
+  const names = ['Bob','Alice','Vassia'];
+  const loggedIn = true;
+  const name = 'world';
+  const styles = {
+    color: 'red',
+    fontSize: '55px',
+  };
+  return (
+    <>
+      <div className='text-5xl'>App</div>
+      <p style={styles}>Hello {name}!</p>
+      <ul>
+        {names.map((name)=>(
+          <li>{name}</li>
+        ))}
+      </ul>
+      { loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1>}
+    </>
+  );
 };
 export default App;
